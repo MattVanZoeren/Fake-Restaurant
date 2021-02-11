de0a131012609dbe13ba2d4ebf13b69edbb4e825
Matthew VanZoeren <mattvanzoeren@gmail.com>
2021-02-11 14:42:41 -0500

    adding the whole thing at once to github

---
+++ includes/arrays.php
@@ -0,0 +1,81 @@
+<?php
+
+    $navItems = array(
+        array(
+            slug => "index.php",
+            title => "Home"
+        ),
+        array(
+            slug => "team.php",
+            title => "Team"
+        ),
+        array(
+            slug => "menu.php",
+            title => "Menu"
+        ),
+        array(
+            slug => "contact.php",
+            title => "Contact"
+        ),
+    );
+
+
+        $teamMembers = array(
+            array(
+                name => "Frankie III",
+                position => "Owner",
+                bio => "Frankie is the owner and grandson of the original Franklin.",
+                img => "frankie"
+            ),
+            array(
+                name => "Francis",
+                position => "General Manager",
+                bio => "Francis knows her stuff. Big sister to Frankie, she runs the show.",
+                img => "francis"
+            ),
+            array(
+                name => "Carlos",
+                position => "Head Chef",
+                bio => "You won't find a better chef this side of the Nile.",
+                img => "carlos"
+
+            ),
+        );
+
+
+        $menuItems = array(
+            "club-sandwich" => array(
+                title => "Club Sandwich",
+                price => 11,
+                blurb => "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+
+                ,
+                drink => "Club Soda"
+            ),
+            "dill-salmon" => array(
+                title => "Lemon &amp; Dill Salmon",
+                price => 18,
+                blurb => "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+
+                ,
+                drink => "Fancy Wine"
+            ),
+            "super-salad" => array(
+                title => "The Super Salad",
+                price => 34,
+                blurb => "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+
+                ,
+                drink => "Jug o' Water"
+            ),
+            "mexican-barbacoa" => array(
+                title => "Mexican Barbacoa",
+                price => 23,
+                blurb => "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+
+                ,
+                drink => "Beer with lime"
+            ),
+        )
+
+?>
---
+++ includes/header.php
@@ -0,0 +1,31 @@
+<?php
+    $companyName = "Franklin's Fine Dining";
+    include('includes/arrays.php');
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?php echo TITLE ?></title>
+    <link href="/assets/styles.css" rel="stylesheet">
+</head>
+<body id="final-example">
+    
+    <div class="wrapper">
+
+        <div id="banner">
+            <a href="/" title="Return to Home">
+                <img src="img/banner.png" alt="Franklins Fine Dining">
+            </a>
+
+        </div>
+
+        <div id="nav">
+            <?php include('includes/nav.php'); ?>
+        </div>
+
+        <div id="content">
